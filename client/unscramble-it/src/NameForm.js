6ee364c2c668b48c8433b8bc078a147635bf5779
Lydia Tse <lydia@Lydias-MBP.lan>
2019-04-27 21:25:51 -0500

    added display name textfield and created views

---
+++ client/unscramble-it/src/NameForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+/**
+ * NameForm component: renders text box that allows
+ * user to enter a display name
+ */
+class NameForm extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = { value: "" }; // Intializing display name to be empty
+
+    // Binding hte event handlers
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Updating the text box when a change is made
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  // Capturing the entered name
+  handleSubmit(event) {
+    alert("user name typed in was: " + this.state.value);
+    event.preventDefault();
+  }
+
+  // Render the component
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="Your Display Name"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export { NameForm }; // Exporting component
