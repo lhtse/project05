038276161d5c73c27ba315c06a2c7a019ffa9328
Lydia Tse <lydia@Lydias-MacBook-Pro.local>
2019-05-01 18:18:47 -0500

    added game logic skeleton code

--- server/server.js
+++ server/server.js
@@ -1,9 +1,3 @@
-/**
- * TODO:
- * 1. When a client joins, push them into the queue (implemented as an array)
- * 2. When the size of the array is 4, we can start a game
- */
-
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -37,33 +31,48 @@ app.get("/styles.css", function(req, res) {
  * Handler for when client connects
  ***********************************/
 io.on("connection", client => {
-  console.log("a client has connected");
+  console.log("a client has connected: " + client.id);
 
   // Handling when a client has disconnected
   client.on("disconnect", () => {
-    console.log("a client has disconnected");
-  });
+    console.log("a client has disconnected " + client.id);
 
-  // Handling when player has joined
-  // Listens for "player joined" string then will display the message
-  client.on("player joined", playerInfo => {
-    console.log("player has joined: " + playerInfo.name);
+    // Removing player from the queue
+    removeFromQueue(client.id);
 
-    // Add the new player object to the player map
-    playerMap.set(playerInfo.name, playerInfo);
+    // Removing the player from the player map
+    playerMap.delete(client.id);
 
-    // Add the new player to the queue
-    queue.push(playerInfo.name);
+    // Handling when player has joined
+    // Listens for "player joined" string then will display the message
+    client.on("player joined", playerInfo => {
+      console.log("player has joined: " + playerInfo.name);
 
-    // Check to see if there are 4 players in the queue
-    if (queue.length == 4) {
-      io.emit("starting game");
-      // Remove first 4 players
-      // Start the game
-    } else {
-      // Alerting that the user should be in waiting room
-      io.send("show waiting room");
-    }
+      // Add the new player object to the player map
+      playerMap.set(client.id, playerInfo);
+      // fixme: delete
+      // playerMap.forEach((value, key) => {
+      //   console.log("name of player is: " + value.name);
+      //   console.log("key is " + key);
+      // });
+
+      // Add the new player to the queue
+      queue.push(client.id);
+      // fixme: delete
+      // queue.forEach(element => {
+      //   console.log("element: " + element);
+      // });
+
+      // Check to see if there are 4 players in the queue
+      if (queue.length == 4) {
+        io.emit("starting game");
+
+        // Remove first 4 players (call this in the play game function)
+        getGamePlayers();
+
+        // Start the game
+      }
+    });
   });
 });
 
@@ -72,3 +81,51 @@ io.on("connection", client => {
  *********************************************/
 var queue = []; // Creating queue using an array that will hold the names of the players
 var playerMap = new Map();
+
+/*********************************
+ * Removes player from the queue
+ ********************************/
+function removeFromQueue(clientID) {
+  var index = queue.indexOf(clientID);
+  queue.splice(index, 1);
+}
+
+/*************************************************
+ * Removes first 4 players from the queue and
+ * stores them into a JSON object
+ ************************************************/
+function getGamePlayers() {
+  var clientID;
+  for (i = 0; i < 4; i++) {
+    clientID = queue.shift();
+    // console.log("removing: " + playerMap.get(clientID).name); //fixme delete
+  }
+
+  console.log("size of queue: " + queue.length); // fixme delete
+}
+
+/*********************************
+ * Plays a round of the game
+ ********************************/
+function playGame() {
+  // Retrieve the object that holds all the current players
+  // var gameInfo = getGamePlayers();
+
+  // Get the scrambled word
+
+  // Start the timer
+
+  // Listen for submitted word
+  io.on("play word", word => {
+    // Check if the word is correct
+    var placeholder = "delete me";
+    // If word is correct then send message to the player of the game
+  });
+}
+
+function sendToGamePlayers(gameInfo, message) {
+  // Retrieve client id for each player in the object
+  // Send message to the specific players
+
+  var placeholder = "delete me";
+}
